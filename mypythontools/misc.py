eff156f64ff6ed4887f90f2ade38f594b1154008
Malachov <malachovd@seznam.cz>
2021-02-02 21:57:56 +0100

    New version

--- mypythontools/misc.py
+++ mypythontools/misc.py
@@ -7,7 +7,6 @@ import sys
 # Root is usually current working directory, if not, use `set_root` function.
 root_path = Path.cwd()
 
-<<<<<<< HEAD
 
 def set_root(set_root_path=None):
     """Root folder is inferred automatically if call is from git_hooks folder or from root (cwd).
@@ -21,13 +20,3 @@ def set_root(set_root_path=None):
 
     if not root_path.as_posix() in sys.path:
         sys.path.insert(0, root_path.as_posix())
-
-
-set_root()
-=======
-if not (root_path / 'docs').exists():
-    root_path = root_path.parent
-
-if not root_path.as_posix() in sys.path:
-    sys.path.insert(0, root_path.as_posix())
->>>>>>> f8f3ed5e8a279d7610b8706fe2f86736f9129a78
