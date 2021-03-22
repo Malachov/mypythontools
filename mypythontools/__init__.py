6f9e9d706e2664aa3b965a74034ad27a6d329229
Malachov <malachovd@seznam.cz>
2021-03-22 10:02:08 +0100

    sdf

--- mypythontools/__init__.py
+++ mypythontools/__init__.py
@@ -9,13 +9,13 @@ Official documentation - https://mypythontools.readthedocs.io/
 Official repo - https://github.com/Malachov/mypythontools
 
 Usually used from IDE. Used paths are infered and things like building the
-application with pyinstaller incrementing version, generating rst files for sphinx docs,
+application with pyinstaller, incrementing version, generating rst files for sphinx docs,
 pushing to github or deploying to Pypi is matter of calling one function
 or clicking one button (e.g. Vs code task).
 
 Many projects - one codebase.
 
-If you are not sure whether structure of app that will work with this code, there is python starter repo
+If you are not sure whether structure of your app will work with this code, there is python starter repo
 on https://github.com/Malachov/my-python-starter
 
 Paths are infered, but if you have atypical structure or have more projects in cwd, use `mypythontools.misc.set_paths()`.
