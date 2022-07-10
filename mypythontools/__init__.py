65f6a14d0dc42785970fdffb6a40410b45da33ba
malachov <malachovd@seznam.cz>
2022-07-10 20:49:17 +0200

    New version

--- mypythontools/__init__.py
+++ mypythontools/__init__.py
@@ -137,7 +137,7 @@ from mypythontools import config, misc, paths, plots, property, system, types
 
 __all__ = ["config", "misc", "paths", "plots", "property", "system", "types"]
 
-__version__ = "2.1.2"
+__version__ = "2.1.4"
 
 __author__ = "Daniel Malachov"
 __license__ = "MIT"
